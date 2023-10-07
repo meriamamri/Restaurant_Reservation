10bc4cf90e688e61a5aaf814e1b9cdc26c472472
meriamamri <meriem919@gmail.com>
2023-10-07 11:46:16 +0100

    fetch menu data

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,15 +1,28 @@
+import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-const Menu = () => {
+interface MenuProps {
+  items: Item[];
+}
+
+const Menu = (props: MenuProps) => {
+  const { items } = props;
+
+  const renderMenuCard = () => {
+    if (items.length > 0) {
+      return items.map((item: Item) => <MenuCard key={item.id} item={item} />);
+    } else {
+      return <p>This restaurant does not have a menu</p>;
+    }
+  };
+
   return (
     <main className="bg-white mt-5">
       <div>
         <div className="mt-4 pb-1 mb-1">
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
-        <div className="flex flex-wrap justify-between">
-          <MenuCard />
-        </div>
+        <div className="flex flex-wrap justify-between">{renderMenuCard()}</div>
       </div>
     </main>
   );
--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -2,13 +2,14 @@ import Header from "./components/Header";
 
 interface RestaurantLayoutProps {
   children: React.ReactNode;
+  params: { slug: string };
 }
 
 const RestaurantLayout = (props: RestaurantLayoutProps) => {
-  const { children } = props;
+  const { children, params } = props;
   return (
     <main>
-      <Header />
+      <Header name={params.slug} />
       <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
         {children}
       </div>
